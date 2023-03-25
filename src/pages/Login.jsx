a1ed4ae26762d64c4444bb2b90c00f5d6a835bd5
@Viena <vbaca1977@gmail.com>
2023-03-24 18:11:17 -0600

    agregando useRef

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import '@styles/Login.scss';
 
+import logoYard from '@logos/logo_yard_sale.svg';
+
 const Login = () => {
+    const formulario = useRef(null);
+
+    const handleSubmit = ()=> {
+        event.preventDefault();
+        const formData = new FormData(formulario.current);
+        const data = {
+            usename: formData.get('email'),
+            password: formData.get('password')
+        }
+        console.log(data);
+    }
+
     return (
         <div className ="login">
         <div className ="form-container">
-            <img src="./logos/logo_yard_sale.svg" alt="logo" class="logo" />
+            <img src={logoYard} alt="logo" className ="logo" />
 
-            <form action="/" className ="form">
+            <form action="/" className ="form" ref={formulario}>
                 <div>
-                    <label for="email" className ="label">Email address</label>
-                    <input type="email" id="email" placeholder="user@example.com" className ="input input-email" />
-                    <label for="password" className ="label">Password</label>
-                    <input type="password" id="password" placeholder="*********" className ="input input-password" />
-                    <input type="submit" value="Login" className ="primary-button login-button" />
+                    <label htmlFor="email" className ="label">Email address</label>
+                    <input type="email" name ="email" placeholder="user@example.com" className ="input input-email" />
+                    <label htmlFor="password" className ="label">Password</label>
+                    <input type="password" name ="password" placeholder="*********" className ="input input-password" />
+                    <button type="submit" className ="primary-button login-button" onClick ={handleSubmit}>Log in</button>
                     <p className ="forgot-password">
                         <a href="./new-pass.html">Forgot my password</a>
                     </p>
                 </div>
-                    <input type="submit" value="Sign up" className ="secondary-button signup-button" />
+                    <button className ="secondary-button signup-button">Sign up</button>
             </form>
         </div>
     </div>
